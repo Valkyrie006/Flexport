95f70671c9aa2f538b21578e96639fb07dbd7488
Valkyrie006 <conespeter81@gmail.com>
2024-07-04 18:23:51 +0530

    repo

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package main.java.org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String test = sc.nextLine();
+        System.out.println(test);
+    }
+}
