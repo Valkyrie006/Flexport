b6f68ea87581c2e146e1b32f9c8d754541787276
Valkyrie006 <conespeter81@gmail.com>
2024-07-15 10:27:13 +0530

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,12 @@
 package main.java.org.example;
 
-import java.util.Scanner;
+import java.util.*;
+
 
 public class Main {
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        String test = sc.nextLine();
-        System.out.println(test);
+        // test
 
     }
+
 }
